833dc14dc557ae506202e57271618be9a28efe3b
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-15 22:14:18 -0400

    Initial Commit

---
+++ NavigationStackTutorial/NavigationStackTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTutorialApp.swift
+//  NavigationStackTutorial
+//
+//  Created by Jason Cox on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
