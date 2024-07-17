2d8a2b3a506db30629bb4daf5f356eb0485e0aa6
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-16 22:21:35 -0400

    Initial commit

--- NavigationStackTutorial/ContentView.swift
+++ NavigationStackTutorial/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+struct CarBrand: Identifiable, Hashable {
+    let name: String
+    let id = NSUUID().uuidString
+}
+
+struct Car: Identifiable, Hashable {
+    let id = NSUUID().uuidString
+    let make: String
+    let model: String
+    let year: Int
+    
+    var description: String {
+        return "\(year) \(make) \(model)"
+    }
+}
+
 struct ContentView: View {
+    let brands: [CarBrand] = [
+        .init(name: "Ferrari"),
+        .init(name: "Lamborghini"),
+        .init(name: "BMW"),
+        .init(name: "Mercedes"),
+        .init(name: "Aston Martin")
+    ]
+    
+    let cars: [Car] = [
+        .init(make: "Ferrari", model: "488", year: 2022),
+        .init(make: "Lamborghini", model: "Aventador", year: 2023),
+        .init(make: "BMW", model: "M5", year: 2024),
+        .init(make: "Mercedes", model: "AMG 63", year: 2019),
+        .init(make: "Aston Martin", model: "Vantage", year: 2021)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Makes") {
+                    ForEach(brands) { brand in
+                        NavigationLink(value: brand) {
+                            Text(brand.name)
+                        }
+                    }
+                }
+            }
+        
+            .navigationDestination(for: CarBrand.self) { brand in
+                Text("New \(brand.name)")
+            }
         }
-        .padding()
     }
 }
 
